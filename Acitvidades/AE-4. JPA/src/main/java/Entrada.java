7af1c26c9ac5e6a2a204f755064fec96e5894a42
Ivan Núñez Rodriguez <ivannr20@gmail.com>
2025-02-18 11:56:08 +0100

    1

--- Acitvidades/AE-4. JPA/src/main/java/Entrada.java
+++ Acitvidades/AE-4. JPA/src/main/java/Entrada.java
@@ -11,25 +11,25 @@ public class Entrada {
     public static void main(String[] args) {
         LibreriaController libreriaController = new LibreriaController();
 
-//        // Agregar autores
-//        Autor autor1 = new Autor("Gabriel", "García Márquez", "1927-03-06");
-//        Autor autor2 = new Autor("Isabel", "Allende", "1942-08-02");
-//        libreriaController.agregarAutor(autor1);
-//        libreriaController.agregarAutor(autor2);
-//
-//        // Agregar editoriales
-//        Editorial editorial1 = new Editorial("RBA", "Madrid, España");
-//        Editorial editorial2 = new Editorial("Planeta", "Barcelona, España");
-//        libreriaController.agregarEditorial(editorial1);
-//        libreriaController.agregarEditorial(editorial2);
-//
-//        // Agregar libros
-//        libreriaController.agregarLibro(new Libro("Cien años de soledad", 25.99), 1, 1);
-//        libreriaController.agregarLibro(new Libro("La casa de los espíritus", 20.50), 2, 2);
-//
-//        // Agregar librerías
-//        Libreria libreria1 = new Libreria("Librería Central", "Juan Pérez", "Calle Mayor, 10");
-//        libreriaController.agregarLibreria(libreria1, Arrays.asList(1, 2));
+        // Agregar autores
+        Autor autor1 = new Autor("Gabriel", "García Márquez", "1927-03-06");
+        Autor autor2 = new Autor("Isabel", "Allende", "1942-08-02");
+        libreriaController.agregarAutor(autor1);
+        libreriaController.agregarAutor(autor2);
+
+        // Agregar editoriales
+        Editorial editorial1 = new Editorial("RBA", "Madrid, España");
+        Editorial editorial2 = new Editorial("Planeta", "Barcelona, España");
+        libreriaController.agregarEditorial(editorial1);
+        libreriaController.agregarEditorial(editorial2);
+
+        // Agregar libros
+        libreriaController.agregarLibro(new Libro("Cien años de soledad", 25.99), 1, 1);
+        libreriaController.agregarLibro(new Libro("La casa de los espíritus", 20.50), 2, 2);
+
+        // Agregar librerías
+        Libreria libreria1 = new Libreria("Librería Central", "Juan Pérez", "Calle Mayor, 10");
+        libreriaController.agregarLibreria(libreria1, Arrays.asList(1, 2));
 
 
         // Mostrar información
